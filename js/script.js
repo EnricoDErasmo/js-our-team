4c1182e73081dd76b720937507f5acfca57b7418
EnricoDErasmo <enrico.derasmo@gmail.com>
2023-03-08 15:45:23 +0100

    Milestone 0

--- js/script.js
+++ js/script.js
@@ -17,4 +17,48 @@ Trasformare la stringa foto in una immagine effettiva
 BONUS 2:
 Organizzare i singoli membri in card/schede
 */
+// -------------------------------------------------------------------------
+
+
+// MILESTONE 0:
+// Creare l’array di oggetti con le informazioni fornite.
+
+const team = [
+    {
+        nome : "Wayne Barnett",
+        lavoro : "Founder & CEO",
+        foto : "wayne-barnett-founder-ceo.jpg"
+
+    },
+    {
+        nome : "Angela Caroll",
+        lavoro : "Chief Editor",
+        foto : "angela-caroll-chief-editor.jpg"
+
+    },
+    {
+        nome : "Walter Gordon",
+        lavoro : "Office Manager",
+        foto : "walter-gordon-office-manager.jpg"
+
+    },
+    {
+        nome : "Angela Lopez",
+        lavoro : "Social Media Manager",
+        foto : "angela-lopez-social-media-manager.jpg"
+
+    },
+    {
+        nome : "Scott Estrada",
+        lavoro : "Developer",
+        foto : "scott-estrada-developer.jpg"
+
+    },
+    {
+        nome : "Barbara Ramos",
+        lavoro : "Graphic Designer",
+        foto : "barbara-ramos-graphic-designer.jpg"
+
+    }
+];
 
